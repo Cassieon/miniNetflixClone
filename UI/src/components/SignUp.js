1a73364f44471c379c39a6ca8ce9972a29e6ebc3
KenasiaTurner <kenasi123@gmail.com>
2022-05-18 13:44:55 -0400

    adds the template pages

---
+++ UI/src/components/SignUp.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+
+class SignUp extends React.Component {
+    render() {
+        return (
+            <div style = {{background:'magenta', width: '100vw', height: '100vh'}}>
+                <h1 style={{color: 'white'}}>About</h1>
+            </div>
+        )
+    }
+} 
+
+export default SignUp;
