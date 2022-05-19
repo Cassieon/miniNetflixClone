2c2e3e01ad1d2f6f0ff6abeb24dd554cb3aca83a
KenasiaTurner <kenasi123@gmail.com>
2022-05-19 16:15:33 -0400

    adds the navbar

--- UI/src/components/navbar.jsx
+++ UI/src/components/navbar.jsx
@@ -2,26 +2,43 @@ import './navbar.css'
 import SearchIcon from '@mui/icons-material/Search';
 import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import { useState } from 'react'; 
+
 
 const Navbar = () => {
+
+  const [isScrolled,setScrolled] = useState(false); 
+
+  window.onscroll = () => {
+    setScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
+
   return (
-    <div className="navbar">
+    <div className= {isScrolled ? "navbar scrolled ": "navbar"}>
        <div className='container'>
             <div className='left'>
                 <img src="https://www.edigitalagency.com.au/wp-content/uploads/netflix-logo-png-large.png" alt="logo"/>
-            </div>
             <span>Home</span>
             <span>TV Shows</span>
             <span>Movies</span>
             <span>News & Popular</span> 
             <span>My List</span>
+            </div>
             <div className='right'>
-                <SearchIcon/>
-                <span>KID</span>
-                <span>DVD</span>
-                <NotificationsActiveIcon/>
-                <img src="https://pbs.twimg.com/media/DmtcXxYUcAYshhQ?format=jpg&name=240x240" alt="profile"/>
-                <ArrowDropDownIcon/>
+                <SearchIcon className='icon'/>
+                  <span>KID</span>
+                  <span>DVD</span>
+                <NotificationsActiveIcon className='icon'/>
+                  <img src="https://pbs.twimg.com/media/DmtcXxYUcAYshhQ?format=jpg&name=240x240" alt="profile"/>
+                <div className='profile'>
+                <ArrowDropDownIcon className='icon'/>
+                  <div className='options'>
+                    <span>Settings</span>
+                    <span>Logout</span>
+                    </div>
+                </div>
             </div>
        </div>
     </div>
